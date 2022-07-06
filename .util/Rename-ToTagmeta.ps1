8b67d4e2a6cd1d51e220a08fb34d033527634331
Raiden Erdmann <raiden.erdmann@gmail.com>
2022-07-06 13:25:42 +0200

    fix: Rename Items with special character in filename

--- .util/Rename-ToTagmeta.ps1
+++ .util/Rename-ToTagmeta.ps1
@@ -6,17 +6,31 @@ function Rename-ToTagMeta {
         $FilePath
     )
 
-    if($FilePath -isnot [System.IO.FileInfo]){
-        $FilePath = Get-Item $FilePath
+    if ($FilePath -isnot [System.IO.FileInfo]) {
+        $FilePath = Get-Item -LiteralPath $FilePath
     }
-    if(!$global:TagLibLoaded){
+    if (!$global:TagLibLoaded) {
         Add-Type -Path "$PSScriptRoot\TagLibSharp.dll"
         $global:TagLibLoaded = $true
     }
 
-    $tags = [TagLib.File]::Create($FilePath.FullName)
+    try {
+        Write-Verbose "Creating $FilePath"
+        $tags = [TagLib.File]::Create($FilePath.FullName)
+    } catch {
+        throw $_
+    }
     $mime = $tags.MimeType.Split("/")[-1]
+    if (!$tags.Tag.Artists) {
+        Write-Warning "No artists: {$FilePath}"
+        return
+    }
+    if (!$tags.Tag.Title) {
+        Write-Warning "No Title: {$FilePath}"
+        return
+    }
     $NewName = "$($tags.Tag.Artists -join ', ') - $($tags.Tag.Title).$mime"
 
+    $NewName = $NewName.Split([IO.Path]::GetInvalidFileNameChars()) -join '_'
     Rename-Item -LiteralPath $FilePath.FullName -NewName $NewName
 }
--- load.ps1
+++ load.ps1
@@ -1,8 +1,18 @@
 [CmdletBinding()]
-param ()
+param (
+    [switch]$Force
+)
 
 if ($MyInvocation.InvocationName -ne ".") {
     Write-Warning "To Export functions to your session, you need to dot source scripts! "
+}
+if ($Force -or !$global:TagLibLoaded) {
+    if (Test-Path "$PSScriptRoot\.util" -PathType Container) {
+        Add-Type -Path "$PSScriptRoot\.util\TagLibSharp.dll"
+    } else {
+        Add-Type -Path  "$PSScriptRoot\TagLibSharp.dll"
+    }
+    $global:TagLibLoaded = $true
 }
 Write-Host "Loading scripts"
 $scripts = Get-ChildItem -Path "$PSScriptRoot\.util" -File -Filter "*.ps1" | Where-Object { $_.FullName -ne $PSCommandPath }
