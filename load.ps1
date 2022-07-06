93dd0a5d83efde63be1b0eff9c042e3bfd664413
Raiden Erdmann <raiden.erdmann@gmail.com>
2022-07-06 12:20:38 +0200

    Load ps reminds of dot sourcing

--- load.ps1
+++ load.ps1
@@ -1,7 +1,12 @@
 [CmdletBinding()]
 param ()
 
+if ($MyInvocation.InvocationName -ne ".") {
+    Write-Warning "To Export functions to your session, you need to dot source scripts! "
+}
 Write-Host "Loading scripts"
-$scripts = Get-ChildItem -Path "$PSScriptRoot\.util" -File -Filter "*.ps1"| Where-Object { $_.FullName -ne $PSCommandPath }
-$scripts
-$scripts | ForEach-Object { Write-Verbose "Loading $_"; . "$($_.FullName)" }
+$scripts = Get-ChildItem -Path "$PSScriptRoot\.util" -File -Filter "*.ps1" | Where-Object { $_.FullName -ne $PSCommandPath }
+
+$scripts | ForEach-Object { Write-Verbose "Loading $_"; . ($_.FullName) }
+
+Write-Host "Done" -ForegroundColor Green
