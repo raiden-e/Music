23dcc9b9beb120eb96497dd5de6878214a1a59db
Raiden Erdmann <raiden.erdmann@gmail.com>
2022-07-06 12:40:23 +0200

    fix: Convert-Music feat: Convert-Music -Format "flac"

--- .util/Convert-Music.ps1
+++ .util/Convert-Music.ps1
@@ -5,7 +5,7 @@ function Convert-Music {
         [Parameter(Mandatory, ValueFromPipeline)]
         $InputObject,
         [Parameter(Mandatory)]
-        [ValidateSet("mp3", "m4a")]
+        [ValidateSet("mp3", "m4a", "flac")]
         $Format,
         $Artwork,
         $Bitrate = 320
@@ -23,11 +23,6 @@ function Convert-Music {
         Write-Warning "Converting from a lossy format into another is SUPER lossy!"
     }
 
-    # if (!$global:TagLibLoaded) {
-    #     Add-Type -Path "$PSScriptRoot\TagLibSharp.dll"
-    #     $global:TagLibLoaded = $true
-    # }
-
     $ffmpeg = Get-Item "$PSScriptRoot\ffmpeg.exe" -ErrorAction SilentlyContinue
     if (!$ffmpeg) {
         . "$PSScriptRoot\Update-FFMPEG.ps1"
@@ -36,35 +31,33 @@ function Convert-Music {
 
     $output = [IO.Path]::Combine($InputObject.Directory, "$($InputObject.BaseName).$Format")
 
-    $arg = "-hide_banner -i '$($in.FullName)'"
+    $ffmpegArguments = "-hide_banner", "-i", "'$($InputObject.FullName)'"
     if ($Artwork) {
         $Artwork = Get-Item $Artwork -ErrorAction SilentlyContinue
         if ($Artwork -isnot [IO.FileInfo]) {
             Write-Warning "Not a file: $Artwork"
         } else {
-            $arg += " -i '$($Artwork.FullName)' -map 0 -map 1 -disposition:v:1 attached_pic"
+            $ffmpegArguments += "-i", "'$($Artwork.FullName)'", "-map", "0", "-map", "1", "-disposition:v:1", "attached_pic"
         }
     }
-    $arg += " -b:a ${Bitrate}000 "
+
+    $ffmpegArguments += "-b:a", "${Bitrate}000"
 
     if ($artist -or $title -or $album) {
         if ($artist) {
-            $arg += " -metadata artist="""
-            foreach ($a in $artist) {
-                $arg += "$a/"
-            }
-            $arg = $arg.TrimEnd("/")
-            $arg += '"'
+            $ffmpegArguments += "-metadata", "artist=""$($artist -join "/")"""
         }
         if ($title) {
-            $arg += " -metadata title=""$title"""
+            $ffmpegArguments += "-metadata", "title=""$title"""
         }
         if($album){
-            $arg += " -metadata album=""$album"""
+            $ffmpegArguments += "-metadata", "album=""$album"""
         }
     }
 
-    $arg += " ""$outputFileName"" -y"
+    $ffmpegArguments += """$output""", "-y"
+    $ffmpegArguments = $ffmpegArguments -join " "
 
-    $rc = Start-Process $
+    Write-Verbose "Executing: {$ffmpeg $ffmpegArguments}"
+    Invoke-Expression "$ffmpeg $ffmpegArguments"
 }
