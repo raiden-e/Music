73ffb3fede740abe0c57aa4539e7c7162eb72744
Raiden Erdmann <raiden.erdmann@gmail.com>
2022-12-21 18:52:51 +0100

    No venv

--- .util/Import-Playlist.ps1
+++ .util/Import-Playlist.ps1
@@ -2,21 +2,26 @@ function Import-Playlist {
     [Alias("ipl")]
     [CmdletBinding()]
     param (
-        $Link,
-        $Folder = "$env:OneDrive\Music"
+        [Parameter(Mandatory)]$Link,
+        $Folder = "$env:OneDrive\Music",
+        [Parameter()][Alias("t")][switch]$test
     )
 
     $oldHome = $pwd
     $autofy = Get-Item "$HOME\source\autofy"
 
-    $null = pip install -U pip virtualenv
     Set-Location $autofy.FullName
-    $null = python -m virtualenv venv
+    if (!(Test-Path "$($autofy.FullName)\venv\Scripts\activate.ps1")) {
+        $host.UI.WriteErrorLine("No virtual environment. Please see GitHub -> config")
+        return
+    }
     ."$($autofy.FullName)\venv\Scripts\activate.ps1"
-    pip install -U autopep8 pylint
-    pip install -U -r .\requirements.txt
 
     Write-Verbose "python LocalizePlaylist.py -f ""$Folder"" -p ""$Link"""
-    python LocalizePlaylist.py -f """$Folder""" -p """$Link"""
+    if ($test) {
+        python LocalizePlaylist.py -f """$Folder""" -p """$Link""" -t
+    } else {
+        python LocalizePlaylist.py -f """$Folder""" -p """$Link"""
+    }
     Set-Location $oldHome
 }
