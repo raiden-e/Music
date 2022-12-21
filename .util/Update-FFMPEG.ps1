16f79f46b4f0f05197759993371bc72c5bce6224
Raiden Erdmann <raiden.erdmann@gmail.com>
2022-12-21 18:49:09 +0100

    feat: Update-FFMPEG

--- .util/Update-FFMPEG.ps1
+++ .util/Update-FFMPEG.ps1
@@ -2,18 +2,28 @@ function Update-FFMPEG {
     [Alias("uf")]
     [CmdletBinding()]
     param (
-        [switch]$PassThru
+        [switch]$PassThru,
+        [switch]$Force
     )
 
-    if ("ffmpeg.exe" -in (Get-ChildItem $PSScriptRoot).Name) {
+    if (!$Force -and ("ffmpeg.exe" -in (Get-ChildItem $PSScriptRoot).Name)) {
         return
     }
 
-    $web = Invoke-WebRequest -UseBasicParsing "https://github.com/BtbN/FFmpeg-Builds/releases/tag/latest"
-    $relativeLink = ($web.links | Where-Object { $_ -like "*ffmpeg-n5.0-latest-win64-lgpl-5.0*" }).href
+    $web = Invoke-RestMethod "https://api.github.com/repos/BtbN/FFmpeg-Builds/releases/latest"
+    $link = ($web.assets | ? { $_.name -like "*ffmpeg-n*-latest-win64-lgpl*" -and $_.Name -notlike "*shared*" })[-1].browser_download_url
 
-    Invoke-WebRequest "https://github.com/$relativeLink" -UseBasicParsing -OutFile "$PSScriptRoot\ffmpeg.exe"
-    # unzip
+    Invoke-WebRequest $link -UseBasicParsing -OutFile "$PSScriptRoot\ffmpeg.zip"
+
+    try {
+        Add-Type -Assembly System.IO.Compression.FileSystem
+        $zip = [IO.Compression.ZipFile]::OpenRead("$PSScriptRoot\ffmpeg.zip")
+        $zippedFile = $zip.entries | ? { $_.name -eq "ffmpeg.exe" }
+        [IO.Compression.ZipFileExtensions]::ExtractToFile($zippedFile, "$PSScriptRoot\ffmpeg.exe", $true)
+    } finally {
+        $zip.Dispose()
+    }
+    Remove-Item "$PSScriptRoot\ffmpeg.zip"
 
     if ($PassThru) {
         return Get-Item "$PSScriptRoot\ffmpeg.exe"
